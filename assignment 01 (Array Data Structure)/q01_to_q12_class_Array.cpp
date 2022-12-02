47ae7d742ce665d671d686cba0b6b1b00c71da92
devendra dhare <devendradhare03@gmail.com>
2022-12-02 15:53:39 +0530

    assignment 01 is complete

--- assignment 01 (Array Data Structure)/q01_to_q12_class_Array.cpp
+++ assignment 01 (Array Data Structure)/q01_to_q12_class_Array.cpp
@@ -4,21 +4,21 @@
 using namespace std;
 
 class Array
-{                                                                    // 1.  Define a class Array to implement array data structure with member variables to
-private:                                                             //     store capacity of array, last index of the last filled block of the array and a pointer to
-    int *ptr = NULL, size = 0, last_index = -1;                      //     hold the address of the first block of the dynamically created array.
-public:                                                              //
-    Array(int size);                                                 // 2.  In question 1, define a parameterised constructor to create an array of specified size.
-    bool Is_Empty() { return last_index == -1 ? true : false; }      // 3.  In question 1, add a method to check whether an array is empty or not by returning True or False.
-    void Append(int element);                                        // 4.  In question 1, define a method to append a new element in the array
-    void Insert(unsigned int index, int element);                    // 5.  In question 1, define a method to insert a new element at specified index
-    void Edit(unsigned int index, int element);                      // 6.  In question 1, define a method to edit an element at specified index.
-    void Delete(unsigned int index);                                 // 7.  In question 1, define a method to delete an element at specified index.
-    bool Is_Full() { return last_index == size - 1 ? true : false; } // 8.  In question 1, define a method to check if the array is full by returning true or false.
-    int get_element(unsigned int index) { return ptr[index]; }       // 9.  In question 1, define a method to get element at specified index.
-    int no_of_elements() { return last_index + 1; }                  // 10. In question 1, define a method to count number of elements present in the array.
-    ~Array() { delete ptr; }                                         // 11. In question 1, define a destructor to deallocate the memory of array.
-    int find(int element);                                           // 12. In question 1, define a method to find an element in the array. Return index if the element found, otherwise return -1.
+{                                                              // 1.  Define a class Array to implement array data structure with member variables to
+private:                                                       //     store capacity of array, last index of the last filled block of the array and a pointer to
+    int *ptr = NULL, size = 0, last_index = -1;                //     hold the address of the first block of the dynamically created array.
+public:                                                        //
+    Array(int size);                                           // 2.  In question 1, define a parameterised constructor to create an array of specified size.
+    bool Is_Empty() { return last_index == -1; }               // 3.  In question 1, add a method to check whether an array is empty or not by returning True or False.
+    void Append(int element);                                  // 4.  In question 1, define a method to append a new element in the array
+    void Insert(unsigned int index, int element);              // 5.  In question 1, define a method to insert a new element at specified index
+    void Edit(unsigned int index, int element);                // 6.  In question 1, define a method to edit an element at specified index.
+    void Delete(unsigned int index);                           // 7.  In question 1, define a method to delete an element at specified index.
+    bool Is_Full() { return last_index == size - 1; }          // 8.  In question 1, define a method to check if the array is full by returning true or false.
+    int get_element(unsigned int index) { return ptr[index]; } // 9.  In question 1, define a method to get element at specified index.
+    int no_of_elements() { return last_index + 1; }            // 10. In question 1, define a method to count number of elements present in the array.
+    ~Array() { delete ptr; }                                   // 11. In question 1, define a destructor to deallocate the memory of array.
+    int find(int element);                                     // 12. In question 1, define a method to find an element in the array. Return index if the element found, otherwise return -1.
     void Display();
 };
 
